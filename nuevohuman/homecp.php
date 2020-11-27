233c6499c6899366f1c4106725386d22feaef088
Omar Bonilla <kptzmusic@gmail.com>
2020-11-27 10:18:11 -0500

    Carga de nueva  In¿magen human

---
+++ nuevohuman/homecp.php
@@ -0,0 +1,171 @@
+<?php 
+session_start();
+if (!isset($_SESSION['idusuario'])) {
+?>
+<META HTTP-EQUIV="REFRESH" CONTENT="0;URL=index.php"> 
+<?php 
+die();
+} 
+require_once('conect/clases.php');
+?>
+<!doctype html>
+<html lang="en">
+  <head><meta http-equiv="Content-Type" content="text/html; charset=gb18030">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v3.8.6">
+    <title>HumanTalent</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/dashboard/">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    <!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+
+
+    <!-- Favicons -->
+<link rel="manifest" href="https://getbootstrap.com/docs/4.4/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="https://getbootstrap.com/docs/4.4/assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="https://humantalentsas.com/misc/favicon.ico">
+<script src="js/js.js"></script>
+<meta name="msapplication-config" content="https://getbootstrap.com/docs/4.4/assets/img/favicons/browserconfig.xml">
+
+<meta name="theme-color" content="#563d7c"/>
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .bg-dark {
+      background-color: #0069d9 !important;
+      font-weight: bold;
+        
+      }
+
+      .btn {
+          text-transform: capitalize !important;
+      }
+
+      .btn.btn-secondary {
+        color: #fff;
+        background-color: #0069d9;
+        border-color: #0062cc;
+        text-transform: capitalize !important;
+
+      }
+
+      .btn.btn-success {
+        color: #fff;
+        background-color: #0069d9;
+        border-color: #0062cc;
+        text-transform: capitalize !important;
+        
+      }
+      .btn btn-primary {
+        color: #fff;
+        background-color: #0069d9;
+        border-color: #0062cc;
+        text-transform: capitalize !important;
+        
+      }
+      h2 {
+        color: #0069d9;
+        font-weight: bold;
+        text-transform: capitalize !important;
+      }
+
+      h5 {
+        color: #0069d9;
+        font-weight: bold;
+      }
+
+      legend {
+        color: #0069d9;
+        font-weight: bold;
+      }
+
+      span {
+        color: #0069d9;
+        font-weight: bold;
+      }
+
+
+     
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/4.4/examples/dashboard/dashboard.css" rel="stylesheet">
+    <script
+  src="https://code.jquery.com/jquery-3.5.0.js"
+  integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="
+  crossorigin="anonymous"></script>
+  
+  </head>
+  <body>
+    <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+  <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="home.php?ctr=home">HUMAN TALENT</a>
+ 
+  <ul class="navbar-nav px-3">
+    <li class="nav-item text-nowrap">
+      <a class="nav-link" href="home.php?ctr=cerrarsesion">Cerrar Sesion</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  <div class="row">
+    <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+      <div class="sidebar-sticky">
+      <?php
+      include('menu.php');
+
+      ?>
+      </div>
+    </nav>
+
+    <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4"> 
+    <?php
+      include('validaciones.php');
+      
+      ?>
+
+    </main>
+  </div>
+</div>
+<br>
+<h2>asdsad</h2>
+<br>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<!--
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    -->
+      <script>window.jQuery || document.write('<script src="https://getbootstrap.com/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script>
+      <script src="https://getbootstrap.com/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.9.0/feather.min.js"></script>
+        <script src="//cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.flash.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.html5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/1.6.4/js/buttons.print.min.js"></script>
+        
+        </body>
+</html>
--- nuevohuman/menu.php
+++ nuevohuman/menu.php
@@ -1,65 +1,68 @@
-<ul class="nav flex-column">
 <?php 
 require_once('conect/clases.php');
 $objconsulta= new consultas();
 
 $menus=$objconsulta->menulateral();
-/*
-var_dump($menu);
-echo "<hr>";
-
-$menus = array (array(
-  "titulo"=>"TTULO SECCION",
-  "datos"=>  array(
-      array("menu"=>"HOLA 1 jajajaj","url"=>"url","img"=>"file"),array("menu"=>"HOLA 2 jajajaj","url"=>"url","img"=>"file")
-      )
-  )
-  );
-  var_dump($menus);*/
-
- for($i=0; $i<count($menus); $i++) {
-     
-  $titulo = $menus[$i]['titulo'];
-  $datos = $menus[$i]['datos'];
-  if(count($datos)>0){
-  ?>
-  <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span><?php echo $titulo;?></span>
-          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-  <?php
-
-        for($j=0; $j<count($datos); $j++) {
-          $label=$datos[$j]['menu'];
-          $url=$datos[$j]['url'];
-          $imagen=$datos[$j]['img'];
-        ?>
-            <li class="nav-item">
-            <a class="nav-link" href="home.php?ctr=<?php echo  $url; ?>">
-              <span data-feather="<?php echo  $imagen; ?>"></span>
-              <?php echo $label;?>
-            </a>
-          </li>
-
-            <?php 
-
-
-       }
- }
-
- }
+//var_export($menus);
 ?>
-<h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-          <span>AJUSTES</span>
-          <a class="d-flex align-items-center text-muted" href="#" aria-label="Add a new report">
-            <span data-feather="plus-circle"></span>
-          </a>
-        </h6>
-        <li class="nav-item">
-            <a class="nav-link" href="home.php?ctr=admon&acc=cambioclave">
-              <span data-feather="file"></span>
-              Cambio Clave </a>
-          </li>
-        </ul>
+
+<ul class="navbar-nav mr-auto">
+<li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Usuario
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                   <a class="dropdown-item" href="home.php?ctr=admon&acc=cambioclave">Cambio Clave</a>
+                   <div class="dropdown-divider"></div>
+                   <a class="dropdown-item" href="home.php?ctr=cerrarsesion">Cerrar Sesion</a>
+                </div>
+            </li>
+
+<?php
+            for($i=0; $i<count($menus); $i++) {
+     
+     $titulo = $menus[$i]['titulo'];
+     $datos = $menus[$i]['datos'];
+     if(count($datos)>0){
+     ?>
+     <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php echo $titulo;?>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <?php 
+                for($j=0; $j<count($datos); $j++) {
+                    $label=$datos[$j]['menu'];
+                    $url=$datos[$j]['url'];
+                    $imagen=$datos[$j]['img'];
+                    ?>
+                    <a class="dropdown-item" href="home.php?ctr=<?php echo  $url; ?>"><?php echo $label;?></a>
+                    <?php 
+                  }
+                 ?>
+                </div>
+            </li>
+     <?php
+   
+           /*for($j=0; $j<count($datos); $j++) {
+             $label=$datos[$j]['menu'];
+             $url=$datos[$j]['url'];
+             $imagen=$datos[$j]['img'];
+           ?>
+               <li class="nav-item">
+               <a class="nav-link" href="home.php?ctr=<?php echo  $url; ?>">
+                 <span data-feather="<?php echo  $imagen; ?>"></span>
+                 <?php echo $label;?>
+               </a>
+             </li>
+   
+               <?php 
+   
+   
+          }*/
+    }
+   
+    }
+?>
+             
+          </ul>
